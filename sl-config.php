4999d30b9abb2f05427743deabe0f942b59c53b2
OptimalDevs <OptimalDevs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-06 11:18:26 +0000

    Tag 2.4
    git-svn-id: https://plugins.svn.wordpress.org/sexy-login/tags/2.4@752229 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sl-config.php
@@ -0,0 +1,12 @@
+<?php 
+
+global $wpdb;
+define( 'SL_NONCE_SECURITY', 'sexy-login-nonce' );
+define( 'SL_LOGIN_TABLE',  $wpdb->prefix . 'sexy_login' );
+define( 'SL_LOGIN_ATTEMPTS', 3 );
+define( 'SL_LOGIN_ATTEMPTS_LAPSE', 10 );
+define( 'SL_LOSTPWD_TIME_LAPSE', 10 );
+define( 'SL_VERSION', '2.4' );
+define( 'SL_PLUGIN_ROOT_URL', plugin_dir_url( __FILE__ ) );
+
+?>
